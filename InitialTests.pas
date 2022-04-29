47056002e28bd96311bdac2c9b198f5111281cf7
Richard Hatherall <richard@appercept.com>
2022-04-29 10:44:40 +0100

    Initial commit

---
+++ InitialTests.pas
@@ -0,0 +1,19 @@
+﻿unit InitialTests;
+
+interface
+
+uses
+  DUnitX.TestFramework;
+
+type
+  [TestFixture]
+  TInitialTests = class
+  public
+  end;
+
+implementation
+
+initialization
+  TDUnitX.RegisterTestFixture(TInitialTests);
+
+end.
